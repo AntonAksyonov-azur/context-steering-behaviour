27d8f0915a4cba9f6bc453cac792ecf418a557ff
arazect <za3a24@yandex.ru>
2026-05-25 23:53:36 +0300

    газ

--- Assets/Scripts/ContextSteering/Behaviours/ContextSteeringBehaviourAvoidance.cs
+++ Assets/Scripts/ContextSteering/Behaviours/ContextSteeringBehaviourAvoidance.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace ContextSteering.Behaviours
@@ -8,11 +9,19 @@ namespace ContextSteering.Behaviours
         // Debug
         public readonly List<Vector3> DebugHitPoints = new List<Vector3>();
         
-        private readonly float _avoidDistance;
+        private readonly float _avoidDistance; 
+        private readonly ContactFilter2D _contactFilter2D;
+        private readonly List<RaycastHit2D> _raycastHits = new  List<RaycastHit2D>();
 
         public ContextSteeringBehaviourAvoidance(int resolution, float avoidDistance) : base(resolution)
         {
             _avoidDistance = avoidDistance;
+            
+            _contactFilter2D = new ContactFilter2D
+            {
+                // useLayerMask = true,
+                // layerMask = LayerMask.GetMask("Obstacle")
+            };
         }
 
         #region Overrides of AbstractContextSteeringBehaviour
@@ -39,18 +48,24 @@ namespace ContextSteering.Behaviours
                 // Detect obstacle
                 var direction = directions[i];
                 
-                var hit = Physics2D.Raycast(agentTransform.position, direction, _avoidDistance);
-                if (hit.collider == null)
+                var hits = Physics2D.Raycast(agentTransform.position, direction, _contactFilter2D, _raycastHits, _avoidDistance);
+                if (hits == 0)
+                {
+                    continue;
+                } 
+
+                var firstObstacleHit = _raycastHits.FirstOrDefault(a => a.transform != agentTransform);
+                if (firstObstacleHit.transform == null)
                 {
                     continue;
                 }
-
-                var vectorHitPointDirection = (Vector3)hit.point - agentTransform.position;
+                
+                var vectorHitPointDirection = (Vector3)firstObstacleHit.point - agentTransform.position;
                 var vectorHitPointDirectionNormalized = vectorHitPointDirection.normalized;
                 danger[i] = Vector3.Dot(direction, vectorHitPointDirectionNormalized);
                 
                 // Debug
-                DebugHitPoints.Add(hit.point);
+                DebugHitPoints.Add(firstObstacleHit.point);
             }
         }
 
